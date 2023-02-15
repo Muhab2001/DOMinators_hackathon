cbde4a720b77682becf63d8d6ebe420c56f644ae
Muhab Abubaker <mohababubakir2001@gmail.com>
2023-02-15 14:15:39 +0300

    first commit

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextTranslate = require('next-translate');
+const nextConfig = {
+  reactStrictMode: true,
+}
+
+module.exports = nextTranslate(nextConfig)
