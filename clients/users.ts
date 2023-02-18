579d24065ff8cd20b22aa300629638daab6b701d
ammar-faifi <a@ammarf.com>
2023-02-18 17:35:11 +0300

    add users API

--- clients/users.ts
+++ clients/users.ts
@@ -1,13 +1,18 @@
 import { UserRole } from '@/stores/profile'
 import { Logout } from 'tabler-icons-react'
+import axios from 'axios';
 
 export class UserClient {
   static async login(username: string, password: string): Promise<UserProfile> {
+    const user = (await axios.get('http://localhost:8000/api/users/current/')).data;
+    const profile = (await axios.get(`http://localhost:8000/api/user_profile/${user.pk}/`)).data;
+
+
     // do some fetching
     return {
-      name: 'John Doe',
-      email: '',
-      avatar: '',
+      name: user.username,
+      email: user.email,
+      avatar: profile.profile_pic,
       role: UserRole.GUEST,
     }
   }
