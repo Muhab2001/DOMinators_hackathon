2ea5d8e96061eca6893e5c170b7adbaf55f463ea
Bashar <bashar.alhaies@gmail.com>
2023-02-18 17:44:27 +0300

    voice pdf with bugs

--- components/InvoiceDownload.tsx
+++ components/InvoiceDownload.tsx
@@ -4,17 +4,21 @@ import dynamic from 'next/dynamic'
 import { useEffect, useState } from 'react'
 import InvoicePDF from './InvoicePDF'
 
-const InvoiceDownload = ({ invoiceData }) => {
+const InvoiceDownload = ({ activityData, invoicesData }) => {
   const [client, setClient] = useState(false)
 
   useEffect(() => {
     setClient(true)
+    console.log('in use effect')
+    console.log(invoicesData)
   }, [])
 
   return (
     <PDFDownloadLink
-      document={<InvoicePDF invoice={invoiceData} />}
-      fileName={`${invoiceData.title}.pdf`}
+      document={
+        <InvoicePDF activityData={activityData} invoicesData={invoicesData}/>
+      }
+      fileName={`${invoicesData.title}.pdf`}
     >
       {({ blob, url, loading, error }) => (
         <Button disabled={loading} color={loading ? 'gray' : 'green'}>
